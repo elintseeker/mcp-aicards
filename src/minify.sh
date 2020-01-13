cbe0fcdf21db099153c2fc149b1f211f652a04d6
kevinn <kevinnavia@gmail.com>
2020-01-13 21:40:27 +0800

    app updates, simplified deck

--- src/minify.sh
+++ src/minify.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-echo Make sure you have SASS and UglifyJS installed as global via npm.
+echo Make sure you have SASS, clean-css, postcss/autoprefixer and UglifyJS installed as global via npm.
 
 sleep 1s
 
 echo Compressing JS
-uglifyjs -c -m -o ../app/app.min.js ../src/app.js
+uglifyjs -c -m -o app/app.min.js src/app.js
 
-sleep 1s
+sleep 0.5s
 
 echo Compressing CSS
-sass ../src/scss/style.scss ../app/style.css
+sass src/scss/style.scss app/style.css &&
+npx postcss app/*.css --use autoprefixer -d app/ &&
+cleancss -o app/style.min.css app/style.css 
